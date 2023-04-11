5d4029ca698a1cf904d38b90c5c381ead1bdc9d2
rahulcb03 <125521382+rahulcb03@users.noreply.github.com>
2023-04-11 17:46:32 -0400

    Add files via upload

--- ttts.c
+++ ttts.c
@@ -51,8 +51,7 @@ int openListener(char *port, int qLen){
 
 		// if we got this far, we have opened the socket
 		break;
-	}
-	freeaddrinfo(info_list);
+	}freeaddrinfo(info_list);
 
 	// info will be NULL if no method succeeded
 	if (info == NULL) {
@@ -62,6 +61,11 @@ int openListener(char *port, int qLen){
 
 	return sock;
 }
+
+void playGame( int sock1, int sock2){
+	//this will handle the actuall playing of the game
+}
+
 #define BUFFLEN 200
 int main(int argc, char ** argv){
 	if(argc != 2){
@@ -71,22 +75,72 @@ int main(int argc, char ** argv){
 
 	struct sockaddr_storage remote_host;
 	socklen_t remote_host_len;
+	char buff[BUFFLEN];
+
+	char holder[100]; 
 	
+	//call openListener to listen to the port, returns a socket FD
 	int listener = openListener(argv[1] , QUEUE_SIZE); 
+
 	if(listener <0){ return EXIT_FAILURE; }
 
 	remote_host_len = sizeof(remote_host);
 	
-	int sock = accept(listener, (struct sockaddr *)&remote_host, &remote_host_len);
+	//get the socket FD connected to the 1st client 
+	int sock1 = accept(listener, (struct sockaddr *)&remote_host, &remote_host_len);
 	
-	if (sock < 0) {
+	if (sock1 < 0) {
 		perror("accept");
 	
 	}
 	
-	char buff[BUFFLEN]; 
-	int byte = read(sock, buff, BUFFLEN); 
-	write(1,buff, byte); 
+		
+	//check if client1 types PLAY 
+	do{
+		read(sock1, buff, BUFFLEN); 
+		buff[4] = '\0'; 
+
+		if(strcmp(buff, "PLAY") ==0){
+			//FIXME: do something with name here
+			 strcpy(holder, "WAIT|0|") ;
+			write(sock1, holder, strlen(holder) ) ; 	
+			 
+		}
+		else{
+			strcpy(holder, "INVL|19|ussage: PLAY <str>|");
+			write(sock1, holder, strlen(holder) ) ; 
+		}
+
+	}while(strcmp(buff, "PLAY") != 0); 
+	
+	//create a socket FD for the second client 
+	int sock2 = accept(listener, (struct sockaddr *)&remote_host, &remote_host_len);
+		
+	if (sock2 < 0) {
+		perror("accept");
+	
+	}
+
+	//check if client2 types PLAY and the name is valid 
+	do{
+		read(sock2, buff, BUFFLEN); 
+		buff[4] = '\0'; 
+
+		if(strcmp(buff, "PLAY") ==0){
+			//FIXME: do something with name here
+			strcpy(holder, "WAIT|0|");	
+			write(sock2, holder, strlen(holder) ) ; 	
+			
+		}
+		else{
+			strcpy(holder,  "INVL|19|ussage: PLAY <str>|");
+			write(sock2, holder, strlen(holder) ) ; 
+		}
+
+	}while(strcmp(buff, "PLAY") != 0); 
+
+	//this will call play game which will handle the game 
+	playGame(sock1, sock2); 	
 
 	return EXIT_SUCCESS; 
 
